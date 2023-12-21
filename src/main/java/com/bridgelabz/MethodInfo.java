bc7f29ed0709dfd025456109022d8b06763e154f
Pasunuri Srinidhi <93040752+PasunuriSrinidhi@users.noreply.github.com>
2023-12-21 14:39:50 +0530

    Created Annotations

--- src/main/java/com/bridgelabz/MethodInfo.java
+++ src/main/java/com/bridgelabz/MethodInfo.java
@@ -1,2 +1,15 @@
-package com.bridgelabz;public interface MethodInfo {
+package com.bridgelabz;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.METHOD)
+@Inherited
+@Retention(RetentionPolicy.RUNTIME
+)
+public @interface MethodInfo {
+    String author() default "Narayan";
+    String date();
+    int revision() default 1;
+    String comments();
 }
